7a0d2130098cc2dca7a6c7024af5bb962c3b490c
nathanfaucett <nathanfaucett@gmail.com>
2016-03-10 21:21:40 -0600

    updates

--- src/create.js
+++ src/create.js
@@ -1,4 +1,5 @@
-var once = require("once"),
+var has = require("has"),
+    once = require("once"),
     isString = require("is_string"),
     isFunction = require("is_function"),
     series = require("./series"),
@@ -11,7 +12,11 @@ module.exports = create;
 function create() {
     var _tasks = {};
 
-    function task(name, fn) {
+    function task(name, description, fn) {
+        if (isFunction(description)) {
+            fn = description;
+            description = "";
+        }
         if (isFunction(name)) {
             fn = name;
             name = fn.displayName || fn.name;
@@ -30,6 +35,9 @@ function create() {
                 throw new TypeError("task function must be specified");
             }
 
+            fn.displayName = name;
+            fn.description = description;
+
             _tasks[name] = fn;
 
             return fn;
@@ -47,10 +55,39 @@ function create() {
         }
     }
 
+    function help(name) {
+        var tasks = _tasks,
+            localHas = has,
+            ret;
+
+        if (name && localHas(tasks, name)) {
+            return taskToString(tasks[name]);
+        } else {
+            ret = "";
+
+            for (name in tasks) {
+                if (localHas(tasks, name)) {
+                    ret += taskToString(tasks[name]);
+                }
+            }
+
+            return ret;
+        }
+    }
+
     task.run = run;
+    task.help = help;
     task.series = series;
     task.parallel = parallel;
     task.create = create;
 
     return task;
+}
+
+function taskToString(fn) {
+    if (!fn.description) {
+        return " - " + fn.displayName + "\n\r";
+    } else {
+        return " - " + fn.displayName + ":\n\r\t\t" + fn.description;
+    }
 }
